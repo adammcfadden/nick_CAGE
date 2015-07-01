e3196570b1e8a5b14a945c51073fd1a9b4ba3352
Ian MacDonald & Adam McFadden <ian@iancmacdonald.com>
2015-07-01 11:35:23 -0700

    adding reviews is passing integration spec

--- spec/features/add_reviews_spec.rb
+++ spec/features/add_reviews_spec.rb
@@ -4,8 +4,8 @@ describe 'The adding reviews path' do
   it 'will let the user add a review to a movie, will display the reviews on the movie page' do
     movie = Movie.create(title: "Con Air", description: "the one with a plane", release_date: "02/09/1995", rating: "8")
     visit movie_path(movie)
-    click_on "Add a Review"
-    expect(page).to have_content 'Review Con Air'
+    click_on "Review Con Air"
+    expect(page).to have_content 'Add a New Review to Con Air'
     fill_in "review_body", with: "Nick Cage is Aaaaamazing"
     fill_in "review_rating", with: "9"
     click_on "Create Review"
@@ -15,8 +15,8 @@ describe 'The adding reviews path' do
   it 'will return an error if a field is left empty' do
     movie = Movie.create(title: "Con Air", description: "the one with a plane", release_date: "02/09/1995", rating: "8")
     visit movie_path(movie)
-    click_on "Add a Review"
-    expect(page).to have_content 'Review Con Air'
+    click_on "Review Con Air"
+    expect(page).to have_content 'Add a New Review to Con Air'
     fill_in "review_rating", with: "9"
     click_on "Create Review"
     expect(page).to have_content 'errors'
