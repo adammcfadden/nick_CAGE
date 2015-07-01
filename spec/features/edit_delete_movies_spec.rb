3847e930cde9800c7b69406fdce090da1a7f6061
Ian MacDonald & Adam McFadden <ian@iancmacdonald.com>
2015-07-01 10:20:15 -0700

    add navbar, set root to movies#index

--- spec/features/edit_delete_movies_spec.rb
+++ spec/features/edit_delete_movies_spec.rb
@@ -12,6 +12,7 @@ describe 'The editing and deleting movies path' do
     click_on 'Edit'
     fill_in 'movie_title', with: 'The Awesome Con Air'
     fill_in 'movie_description', with: 'The one on the plane and Nick Cage being Awesome'
+    click_on 'Update Movie'
     expect(page).to have_content('The Awesome Con Air')
     expect(page).to have_content('The one on the plane and Nick Cage being Awesome')
   end
